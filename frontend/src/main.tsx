ce365ce2fe145a60d6407caf6cf0161315145a00
banshaoking <1209849645@qq.com>
2026-06-12 19:28:23 +0800

    fix: add basename for React Router

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -6,7 +6,7 @@ import "./index.css";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <BrowserRouter>
+    <BrowserRouter basename="/dao-contrib-agent">
       <App />
     </BrowserRouter>
   </React.StrictMode>
